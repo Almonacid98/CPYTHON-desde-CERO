995b8885deb02a3b46d194b58315508c8d848ad2
Almonacid98 <gabriel98almonacid@hotmail.com>
2022-05-29 23:10:19 -0300

    Inicio de CPYTHON desde CERO: primer tema --> TIPOS DE DATOS/VARIABLES/OPERACIONES ARITMETICAS

---
+++ P0 - Tipo de datos - variables - operaciones áritmeticas/practica_0_tipo_de_datos_variables.py
@@ -0,0 +1,57 @@
+#| Tipos de DATOS, VARIABLES y Operaciones Aritméticas
+
+#variables
+
+#Tipos de datos numericos
+
+numero_entero = 1
+print(numero_entero)
+
+
+numero_decimal = 2.46
+print(numero_decimal)
+
+numero_entero = numero_entero + 2
+print(numero_entero)
+
+numero = numero_entero + numero_decimal
+print(numero)
+
+numero_entero += 2
+print(numero_entero)
+
+variable_numerica = 20
+variable_numerica = variable_numerica - 10
+print(variable_numerica)
+
+variable = 10
+variable = variable / variable_numerica
+print(variable)
+
+numero = 2/2 + 3
+print(numero)
+
+multiplicacion = 2*3
+print(multiplicacion)
+multiplicacion = numero_entero * numero_decimal
+print(multiplicacion)
+
+#exponente y modulo
+
+potencia = 2**3
+modulo = 11%2
+print(modulo)
+print(potencia)
+
+# Tipos de datos: Texto
+
+texto = "Esta es una frase"
+print(texto)
+
+text = "texto 'entre comillas' XD"
+
+print(text)
+
+texto_con_formato = """ texto con 
+            formato"""
+print(texto_con_formato)
